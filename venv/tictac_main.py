b62c00de076c8555dee63c8bf97d631dc708a055
sulaimanq99 <47853289+sulaimanq99@users.noreply.github.com>
2021-11-01 23:29:01 +0000

    Added new_board function to create an emtpy 3x3 grid

--- venv/tictac_main.py
+++ venv/tictac_main.py
@@ -1,3 +1,23 @@
 
+
+def new_board():
+    board = []
+    for i in range(0,3):
+        board.append([None,None,None])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    print('test again')
+    new_board()
