4ffe0cb4c61413288cb05c36e8cc67190abe3e44
sulaimanq99 <47853289+sulaimanq99@users.noreply.github.com>
2021-11-02 17:35:00 +0000

    Created find_winning_moves_ai and next_move_winner, it checks if the game can be won by the next move and does that move, if not proceeds randomly

--- venv/tictac_ai.py
+++ venv/tictac_ai.py
@@ -1,5 +1,50 @@
 from tictac_main import new_board, render, get_move, is_legal_move, make_move, check_winner, random_ai
 
+def next_move_winner(board,player):
+    board_transposed = [[board[j][i] for j in range(len(board))] for i in range(len(board[0]))]
+    diagonalL, diagonalR = [], []
+    l, r = 0, 2
+    for i in range(3):
+        if board[i].count(player) == 2 and None in board[i]:
+            x = board[i].index(None)
+            return (i, x)
+        elif board_transposed[i].count(player) == 2 and None in board_transposed[i]:
+            x = board_transposed[i].index(None)
+            return(x, i)
+        diagonalL.append(board[i][l])
+        diagonalR.append(board[i][r])
+        l += 1
+        r -= 1
+        if diagonalL.count(player) == 2 and None in diagonalL:
+            x = diagonalL.index(None)
+            return(x, x)
+        elif diagonalR.count(player) == 2 and None in diagonalR:
+            x = diagonalR.index(None)
+            if x == 0:
+                return(0, 2)
+            elif x == 1:
+                return(x, x)
+            elif x == 2:
+                return(2, 0)
+    return False
+
+
+def finds_winning_moves_ai(board, player):
+    next_move = next_move_winner(board,player)
+    if next_move != False:
+        y = next_move[0]
+        x = next_move[1]
+        board[y][x] = player
+        return board
+    else:
+        return random_ai(board,player)
+
+
+
+
+
+
+
 def game_loop_ai():
     board = new_board()
     move_count = 0
@@ -8,7 +53,7 @@ def game_loop_ai():
     render(board)
     while True:
         player = players[player_key]
-        random_ai(board,player)
+        finds_winning_moves_ai(board,player)
         render(board)
         move_count+=1
         player_key *= -1
@@ -17,4 +62,21 @@ def game_loop_ai():
 
 
 if __name__ == '__main__':
+    '''
+    board = [
+        ['X', 'X', None],
+        [None, 'O', None],
+        ['O', None, None]
+    ]
+    finds_winning_moves_ai(board, 'X')
+    finds_winning_moves_ai(board, 'X')
+    # => should always print (1, 0)
+
+    finds_winning_moves_ai(board, 'O')
+    finds_winning_moves_ai(board, 'O')
+    # => should always print (2, 1)'''
+
+
+
+
     game_loop_ai()
