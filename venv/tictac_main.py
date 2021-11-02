5fb319079465667b5d5066663ec71908edac8053
sulaimanq99 <47853289+sulaimanq99@users.noreply.github.com>
2021-11-02 14:43:49 +0000

    Working on check winner function, needed to complete game loop. Currently works for draws, horizontal and diagonal wins, finishing up for vertical wins, might need to use 'zip' to iterate through two tranposed list at the same time

--- venv/tictac_main.py
+++ venv/tictac_main.py
@@ -13,7 +13,8 @@ def render(board):
         print(str(i)+'|'+' '.join(grid) + '|')
     print(' -------')
 
-def get_move():
+def get_move(player_token):
+    print(player_token + "'s turn")
     X = int(input('Enter your moves x coordinate: '))
     Y = int(input('Enter your moves y coordinate: '))
     player_move = (X,Y)
@@ -26,9 +27,53 @@ def make_move(board,player_move,player_token):
         board[y][x] = player_token
     else:
         print(str(player_move) + ' is already taken, please try again')
-        player_move = get_move()
+        player_move = get_move(player_token)
         return make_move(board,player_move,player_token)
     return board
+
+def game_loop():
+    board = new_board()
+    move_count = 0
+    players = {1:'X', -1:'O'}
+    player_key = 1
+    while True:
+        render(board)
+        player = players[player_key]
+        move = get_move(player)
+        new = make_move(board, move, player)
+        render(new)
+        move_count+=1
+        player_key *= -1
+        #check_winner(board, move_count)
+        if check_winner(board, move_count):
+            return
+
+def check_winner(board, move_count):
+    diagonal = [[],[]]
+    l = 0
+    r = 2
+    board_transposed = [[board[j][i] for j in range(len(board))] for i in range(len(board[0]))]
+    for i,k in board:
+        if i[l]: diagonal[0].append(i[l])
+        if i[r]: diagonal[1].append(i[r])
+        l +=1
+        r -=1
+        if i[0] == True and i.count(i[0]) == 3 :
+            print('Player ' +i[0] + ' wins')
+            return True
+
+    if (len(diagonal[0]) == 3 and  diagonal[0].count(diagonal[0][0]) == 3):
+            print('Player ' + diagonal[0][1] + ' wins')
+            return True
+    elif (len(diagonal[1]) == 3 and diagonal[1].count(diagonal[1][0]) == 3):
+            print('Player ' + diagonal[0][1] + ' wins')
+            return True
+
+
+    if move_count == 9:
+        print('Draw')
+        return True
+
 
 
 
@@ -42,6 +87,9 @@ def make_move(board,player_move,player_token):
 
 
 if __name__ == '__main__':
+    game_loop()
+
+    '''
     board = new_board()
     render(board)
     move = get_move()
@@ -49,4 +97,4 @@ if __name__ == '__main__':
     render(new)
     move = get_move()
     new = make_move(board,move,'O')
-    render(new)
+    render(new)'''
