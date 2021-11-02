28aa0fcdd321984189fa2dc7a3e4e78312001f76
sulaimanq99 <47853289+sulaimanq99@users.noreply.github.com>
2021-11-02 13:44:54 +0000

    Added render function to pretty print the game board

--- venv/tictac_main.py
+++ venv/tictac_main.py
@@ -4,7 +4,14 @@ def new_board():
     board = []
     for i in range(0,3):
         board.append([None,None,None])
+    return board
 
+def render(board):
+    print('  0 1 2 \n -------')
+    for i in range(3):
+        grid = [x if x else ' ' for x in board[i]]
+        print(str(i)+'|'+' '.join(grid) + '|')
+    print(' -------')
 
 
 
@@ -20,4 +27,5 @@ def new_board():
 
 
 if __name__ == '__main__':
-    new_board()
+    a = new_board()
+    render(a)
